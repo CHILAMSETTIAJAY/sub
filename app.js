1bd3a407288b19c395a47afd49c1517d51488fb7
Ajay chilamsetti <ajaychilamsetti@gmail.comm>
2023-09-13 17:32:42 +0530

    Modified sub

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function toggleLoginSection() {
+function toggleLoginSection5() {
     const loginSection = document.getElementById("login");
     if (loginSection.style.display === "none" || loginSection.style.display === "") {
       loginSection.style.display = "block";
@@ -34,4 +34,22 @@ function toggleLoginSection() {
     }
   }
 
+  document.addEventListener("DOMContentLoaded", function() {
+    const faqItems = document.querySelectorAll(".faq-item");
   
+    faqItems.forEach(item => {
+      const question = item.querySelector(".question");
+      const answer = item.querySelector(".answer");
+      const plus = item.querySelector(".bi-plus");
+      const dash = item.querySelector(".bi-dash");
+  
+      question.addEventListener("click", function() {
+        const isAnswerVisible = answer.classList.contains("show");
+        answer.classList.toggle("show");
+        answer.style.display = isAnswerVisible ? 'none' : 'block';
+        dash.style.display = isAnswerVisible ? 'none' : 'block';
+        plus.style.display = isAnswerVisible ? 'block' : 'none';
+        cross.style.display = isAnswerVisible ? 'none' : 'block';
+      });
+    });
+  });
